6196174fbd40f94d861b814442cac38ea8b4bb87
Diogo Fernandes <diogofernandescon@gmail.com>
2014-10-10 13:29:20 +0100

    - I have now implemented a two dimensional array so that I can choose the position of the circles on the grid without having to specify a value everytime. Using a grid I know that the top left most 'box' is array grid[0][0]
    - I have used a class to create the features for each circle in terms of
    their centre coordinates and their colour
    
    - Changed the draw_circle function so that it now takes a circle class
    as a parameter and based on the pre-assigned values for each
    characteristic of the circle it will create a circle with those
    characteristics

--- Flow game project.py
+++ Flow game project.py
@@ -10,6 +10,11 @@ Yellow = (255, 255, 0)
 Orange = (255, 100, 0)
 Green = (0, 255, 0)
 
+# Defining grid positions for circles
+# Two dimension array so that the first group would represent possible coordinates of x and the second possible ones for y - easier to understand
+grid = [[75,225,375,525,675],[75,225,375,525,675]]
+
+  
 pygame.init()
 
 # Setting the grid size through width and height of game window
@@ -27,10 +32,6 @@ done = False
 # Change how fast the screen updates
 clock = pygame.time.Clock()
 
-# The starting position of the circle(s)
-
-circle_x = 50
-circle_y = 50
 
 # ---- Main program loop ------
 
@@ -73,10 +74,62 @@ while not done:
         i += 1 # increment control variable to continue looping
         
     # Creating circles for grid
-    def draw_circle(colour, ctr_x, ctr_y):
-        # Functions draws circle with chosen colour and centre co-ordinates of circle
-        pygame.draw.circle(screen, colour, (ctr_x, ctr_y), 40 , 40)
+    class circle():
+        colour = ()
+        ctr_x = ()
+        ctr_y = ()
+     
+    # Red Circle - changing the called values from array 'grid' will change the position on the screen for the grid
+    RedCircle = circle()
+    RedCircle.colour = Red
+    RedCircle.ctr_x = grid[0][0]
+    RedCircle.ctr_y = grid[1][0]
+    
+    # Blue Circle
+    BlueCircle = circle()
+    BlueCircle.colour = Blue
+    BlueCircle.ctr_x = grid[0][3]
+    BlueCircle.ctr_y = grid[1][4]
+    
+    # Yellow Circle
+    YellowCircle = circle()
+    YellowCircle.colour = Yellow
+    YellowCircle.ctr_x = grid[0][4]
+    YellowCircle.ctr_y = grid[1][1]
+    
+    # Orange Circle
+    OrangeCircle = circle()
+    OrangeCircle.colour = Orange
+    OrangeCircle.ctr_x = grid[0][0]
+    OrangeCircle.ctr_y = grid[1][2]
+    
+    # Green Circle
+    GreenCircle = circle()
+    GreenCircle.colour = Green
+    GreenCircle.ctr_x = grid[0][4]
+    GreenCircle.ctr_y = grid[1][0]
+    
+    
+    
+    
+    # Draws circle(s) on screen
+ 
+    def draw_circle(circle):
+            # Functions draws circle with chosen colour and centre co-ordinates of circle
+            pygame.draw.circle(screen, circle.colour, (circle.ctr_x, circle.ctr_y), 40 , 40)
             
+    
+    draw_circle(RedCircle)
+    draw_circle(BlueCircle)
+    draw_circle(YellowCircle)
+    draw_circle(OrangeCircle)
+    draw_circle(GreenCircle)
+        
+    # Creating circles 
+    
+  
+    
+   
     
     # Update screen with changes
     pygame.display.flip()
