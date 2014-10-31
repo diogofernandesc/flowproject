b2edb6dfc6397cb63fc471cf8af78c68bde86b35
diogofernandesc <diogofernandescon@gmail.com>
2014-10-31 18:15:22 +0000

    Changes:
    - Fully commented code
    - Have been able to stop the line from being drawn at specific
    coordinates but work in progress
    
    - Need to figure out how to remove the line drawn if the user tries to
    draw outside the limits so that they can start again

--- Flow game project.py
+++ Flow game project.py
@@ -1,6 +1,6 @@
 import pygame
 import math
-from operator import pos
+
 # Defining colours for the circles, paths(lines) and grid background
 
 Black = (0 , 0, 0)
@@ -12,8 +12,9 @@ Orange = (255, 100, 0)
 Green = (0, 255, 0)
 
 # Defining grid positions for circles
-# Two dimension array so that the first group would represent possible coordinates of x and the second possible ones for y - easier to understand
-grid = [[75,225,375,525,675],[75,225,375,525,675]]
+''' Two dimension array so that the first group would represent possible coordinates of x 
+and the second possible ones for y - easier to understand'''
+grid = [[75, 225, 375, 525, 675], [75, 225, 375, 525, 675]]
 
   
 pygame.init()
@@ -33,9 +34,11 @@ done = False
 # Change how fast the screen updates
 clock = pygame.time.Clock()
 
+# Fill Screen in Black
 screen.fill(Black)
 
-class grid_circle(pygame.sprite.Sprite): 
+class grid_circle(pygame.sprite.Sprite):
+    # Stationary circles class you need a pair of each colour
     def __init__(self, ctr_x, ctr_y, colour):
         pygame.sprite.Sprite.__init__(self)
         self.ctr_x = ctr_x
@@ -44,18 +47,18 @@ class grid_circle(pygame.sprite.Sprite):
         self.colour = colour
         
     def render(self):
+        # Render method draws circles at the stated position and colour given by the parameters
         pygame.draw.circle(screen, self.colour, (self.ctr_x, self.ctr_y), self.radius, 40)
-        
+
+# Initiate x and y variables for coordinates used by click detection and circle_line class for mouse coordinates 
 global x
 global y
-global colour
-colour = 0, 0, 0
-
-x = 0 
+x = 0
 y = 0
 
 def click_detection(GridCircle):
-    pos = pygame.mouse.get_pos()
+    ''' Check whether the user is clicking within the area of the circle by taking the x and y centre coordinates
+    and subtracting the radius - the user can click up to the last point on the circle'''
     if ((GridCircle.ctr_x - GridCircle.radius) <= x) and ((GridCircle.ctr_x + GridCircle.radius) >= x):
         if ((GridCircle.ctr_y - GridCircle.radius) <= y) and ((GridCircle.ctr_y + GridCircle.radius) >= y):
             return True
@@ -64,22 +67,15 @@ def click_detection(GridCircle):
     
 
 class circle_line():
-    def attributes(self):
-        pos = pygame.mouse.get_pos()
-        self.x = pos[0]
-        self.y = pos[1]
-        x = pos[0]
-        y = pos[1]
+    '''Class for the moving line "circle_line" because the line is made up of individual circles'''
     def Move(self):
+        ''' Move method takes the coordinates of where the mouse is pointing and its own coordinates
+        and draws a circle at that position'''
         pos = pygame.mouse.get_pos()
         self.x = pos[0]
         self.y = pos[1]
-        pygame.draw.circle(screen, colour,(pos[0],pos[1]), 10, 10)
+        pygame.draw.circle(screen, colour, (pos[0], pos[1]), 10, 10)
         pygame.display.flip()
-        ''' I draw extra circles because if the user draws the line very quickly there is large gaps between circles 
-        so it doesn't look like a line, this sort of eliminates that problem, there is probably a better solution'''
-        ''' I draw extra circles because if the user draws the line very quickly there is large gaps between circles 
-        so it doesn't look like a line, this sort of eliminates that problem, there is probably a better solution'''
         
 
 
@@ -89,17 +85,46 @@ moving_line = circle_line()
 
 def movement_checker():
     pos = pygame.mouse.get_pos()
-    while (35 <= pos[1] <= 715):
-        while (35 <= pos[0] <= 715):
-            print("Yes")
-            return True
-   
-        
-# Creating circles for grid
+    ''' ***Work in progress***:
+    Basic principle:
+    - The first if statement checks whether or not the moving line is on the borders if so returns False
+    - Otherwise it's true so it can draw and checks the rest
+    - Following if statements:
+        - It checks whether the mouse is within the range of x-coordinates where at specific y-axis values nothing can be drawn
+        - So if the mouse is at this x-coordinates then the following if statement checks whether the y is at the invalid y values and returns false'''
     
+    
+    if (0 <= pos[1] <= 64) or (686 <= pos[1] <= 750) or (0 <= pos[0] <= 64) or (686 <= pos[0] <= 750):
+        # Can't draw on the outer side of the circles towards the window wall
+        return False
+
+    else:
+        if (116 <= pos[0] <= 184):
+            if (86 <= pos[1] <= 184) or (236 <= pos[1] <= 334) or (386 <= pos[1] <= 484) or (536 <= pos[1] <= 634):
+                print("can't draw")
+                return False
+        elif (266 <= pos[0] <= 334):
+            if (86 <= pos[1] <= 184) or (236 <= pos[1] <= 334) or (386 <= pos[1] <= 484) or (536 <= pos[1] <= 634):
+                print("Can't draw")
+                return False
+        elif (416 <= pos[0] <= 484):
+            if (116 <= pos[1] <= 184) or (266 <= pos[1] <= 334) or (416 <= pos[1] <= 484) or (566 <= pos[1] <= 634):
+                print("can't draw")
+                return False
+        elif (566 <= pos[0] <= 634):
+            if (116 <= pos[1] <= 184) or (266 <= pos[1] <= 334) or (416 <= pos[1] <= 484) or (566 <= pos[1] <= 634):
+                print("Can't draw")
+                return False   
+        
+# ---Creating circles for grid---
+
+# Sprite groups for the circle objects
 circle_grid_list = pygame.sprite.Group()
 all_sprites_list = pygame.sprite.Group()
-    
+
+''' The objects take the x and y coordinates as indexes of the grid array which has a list of values for the possible circle centres
+as created by a 5x5 grid'''
+
 # Red Circle Pair
     
 RedCircle1 = grid_circle(grid[0][3], grid[1][0], Red)
@@ -139,62 +164,87 @@ circle_grid_list.add(RedCircle1, RedCircle2, BlueCircle1, BlueCircle2, GreenCirc
 all_sprites_list.add(RedCircle1, RedCircle2, BlueCircle1, BlueCircle2, GreenCircle1, GreenCircle2, OrangeCircle1, OrangeCircle2, YellowCircle1, YellowCircle2)
 
 
-
-# ---- Main program loop ------
-while not done:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True # Closes the game and exits the loop
-            
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            x, y = event.pos
-            
-            # This event is used to assign values to x and y to check the value of the function in the mouse motion event
-            
-            
-                
-        elif event.type == pygame.MOUSEMOTION:
-            state = pygame.mouse.get_pressed()
-            for circle in circle_grid_list:
-                if click_detection(circle) == True:
-                    colour = circle.colour
-                    if state[0] == 1:
-                        while movement_checker() == True:
-                            moving_line.Move()
-                        
-                            
-    # --- grid built here
+# --- grid built here
     
-
+def build_grid():
+    ''' You need two arrays to define the starting and end positions of the line:
+    Because it draws lines, they take two pairs of x-coordinates; one for the start of the line and one for the end'''
     
     x_grid_pos1 = [150, 0]
-    x_grid_pos2 = [150, 750] # You need two arrays to define the starting and end positions of the line
-    
+    x_grid_pos2 = [150, 750]  
+        
     y_grid_pos1 = [0 , 150]
     y_grid_pos2 = [750, 150]
-    
-    i = 0 # Initiating control variable for the loop
+        
+    i = 0  # Initiating control variable for the loop
     while i != 4:
+        ''' ***Very important***:
+        - This while loop is used to control the intervals in which the lines are drawn for the grid
+        - 750x750 grid so divided by 5 gives 150px intervals
+        - So it will draw the initial line at the initial coordinates above
+        - Then increments by 150 which is the amount of pixels in both directions that the lines will be separated by
+        - i is looped 4 times because 4 lines are drawn in each direction'''
+        
         pygame.draw.line(screen, White, (x_grid_pos1[0], x_grid_pos1[1]), (x_grid_pos2[0], x_grid_pos2[1]))
         x_grid_pos1[0] += 150
         x_grid_pos2[0] += 150 
-        
+            
         # The above code deals with vertical code - incrementing the x values of the coordinates of the lines
         # Means loop will draw a new line 150 pixels ahead of the previous
-        
+            
         pygame.draw.line(screen, White, (y_grid_pos1[0], y_grid_pos1[1]), (y_grid_pos2[0], y_grid_pos2[1]))
         y_grid_pos1[1] += 150
         y_grid_pos2[1] += 150
-        
-        i += 1 # increment control variable to continue looping
+            
+        i += 1  # increment control variable to continue looping
+            
+build_grid()
+
+# ---- Main program loop ------
+''' *** IN PROGRESS *** '''
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True  # Closes the game and exits the loop
+            
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = event.pos 
+            ''' If the user clicks down on the left button the mouse coordinates at that point are assigned to variables 
+            x and y which are used to check the condition of the click_detection function'''
           
+        elif event.type == pygame.MOUSEMOTION:
+            ''' State is assigned to an array for each of three mouse buttons
+            at state[0] it checks the left mouse button'''
+            state = pygame.mouse.get_pressed()
+            
+            for circle in circle_grid_list:
+                # Checks following conditions for every circle in that Sprite group
+                
+                if click_detection(circle) == True:
+                    ''' Checks whether a circle is being clicked
+                    - If so then variable colour is assigned to the colour of the clicked circle
+                    - This is used so that the move method from circle_line class can be called using the colour of the clicked circle'''
+                    colour = circle.colour
+                    
+                    # *** WORK IN PROGRESS ***
+                    if movement_checker() != False:
+                        print("can draw")
+                        if state[0] == 1:
+                            # Checking the left mouse button state, if 1 then button is being clicked or held down
+                            moving_line.Move()
+                            
+                    elif movement_checker() == False:
+                        # Used to stop the move method for the moving_line object from being called if movement checker is false
+                        print("Can't draw")
+                            
+                            
     # Update screen with changes
     pygame.display.flip()
         
     # Define frame rate of game - default at 60
     clock.tick(60)
         
-#Quit game
+# Quit game
 pygame.quit()
 
     
